ea307cb4bcf3bb5dfb644174a7899b356df99427
WePlayFleX <alex.kamai@ya.ru>
2021-11-28 22:20:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class Program
+{
+	public static void Main()
+	{
+		Form form = new Form();
+		form.Text = "Traukin Aliaksandr";
+		form.Show();
+		Application.Run();
+	}
+}
